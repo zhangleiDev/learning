b2062625833fe00de4652920b2c37afc9c0a8c9c
zhanglei <zhang@lei.com>
2019-04-19 18:01:53 +0800

    sql更新

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,12 +1,15 @@
 use my;
 CREATE TABLE `user`
-(
-	`id` int(5) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-	`name` VARCHAR(30) NULL DEFAULT NULL COMMENT '姓名',
-	`age` INT(11) NULL DEFAULT NULL COMMENT '年龄',
-	`email` VARCHAR(50) NULL DEFAULT NULL COMMENT '邮箱',
-   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `user` (
+  `id` int(5) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `name` varchar(30) DEFAULT NULL COMMENT '姓名',
+  `age` int(11) DEFAULT NULL COMMENT '年龄',
+  `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
+  `date` date DEFAULT NULL,
+  `date_time` datetime DEFAULT NULL,
+  `stamp` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8
 
 
 INSERT INTO user (id, name, age, email) VALUES
@@ -14,4 +17,5 @@ INSERT INTO user (id, name, age, email) VALUES
 (2, 'Jack', 20, 'test2@baomidou.com'),
 (3, 'Tom', 28, 'test3@baomidou.com'),
 (4, 'Sandy', 21, 'test4@baomidou.com'),
-(5, 'Billie', 24, 'test5@baomidou.com');
+(5, 'Billie', 24, 'test5@baomidou.com'),
+(6, 'haha', 10, 'fw@r23.cn');
